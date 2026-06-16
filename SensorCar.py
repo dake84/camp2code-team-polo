168cb6d19cd218696a397b248a92186eda68bb7d
dake84 <dake84@gmail.com>
2026-06-16 13:04:43 +0100

    Implementes reference values in config.json and read ir_values from SensorCar

--- SensorCar.py
+++ SensorCar.py
@@ -1,4 +1,18 @@
-import BaseCar
+from basisklassen import Infrared
+from BaseCar import BaseCar
+import time
 
 class SensorCar(BaseCar):
 
+    def __init__(self, references: list = [40, 300, 300, 300, 300]):
+        self._ir = Infrared(references)
+        super().__init__()
+
+if __name__ == '__main__':
+
+    sc = SensorCar()
+    #sc.drive_around()
+
+    print(f"RAW: {sc._ir._read_raw()}")
+    print(f"ANALOG: {sc._ir.read_analog()}")
+    print(f"DIGITAL: {sc._ir.read_digital()}")
