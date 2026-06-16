784fd93683bf01727d5f9bae4543706356d22d12
Markus Welk <markus.welk@volkswagen.de>
2026-06-16 14:57:05 +0200

    Update Fahrmodi

--- BaseCar.py
+++ BaseCar.py
@@ -45,6 +45,15 @@ class BaseCar():
         else:
             raise ValueError("Geschwindigkeit: Wert zwischen -100 und 100 eingeben")
 
+# Direction
+    @property
+    def direction(self):
+        if self.speed > 0:
+            return 1
+        elif self.speed < 0:
+            return -1
+        else:
+            return 0
 
     def drive(self, speed = None, steer = None):
         if speed == None:
@@ -58,13 +67,88 @@ class BaseCar():
     def stop(self):
         self.drive(0,90)
 
+### MW: Test Fahrmodi ###
+
+def test_fahrmodus_1(car):
+    print('Starte Fahrmodus 1')
+    car.stop()
+    time.sleep(1)
+
+    car.drive(30)
+    print(f"3 Sekunden vorwärts. Geschwindigkeit: {car.speed}, Lenkwinkel: {car.steering_angle}")
+    time.sleep(3)
+
+    car.stop()
+    print(f"1 Sekunde Stopp. Geschwindigkeit: {car.speed}, Lenkwinkel: {car.steering_angle}")
+    time.sleep(1)
+    
+    car.drive(-30)
+    print(f"3 Sekunden rückwärts. Geschwindigkeit: {car.speed}, Lenkwinkel: {car.steering_angle}")
+    time.sleep(3)
+
+    car.stop()
+    print(f"Fahrmodus 1 beendet. Geschwindigkeit: {car.speed}, Lenkwinkel: {car.steering_angle}")
+
+def test_fahrmodus_2_rechts(car):
+    print('Starte Fahrmodus 2 (Rechts/Uhrzeigersinn)')
+    car.stop()
+    time.sleep(1)
+
+    car.drive(30)
+    print(f"1 Sekunde vorwärts. Geschwindigkeit: {car.speed}, Lenkwinkel: {car.steering_angle}")
+    time.sleep(1)
+
+    car.drive(30, 135)
+    print(f"8 Sekunden rechts vorwärts. Geschwindigkeit: {car.speed}, Lenkwinkel: {car.steering_angle}")
+    time.sleep(8)
+
+    car.stop()
+    print(f"Kurzer Zwischenstopp. Geschwindigkeit: {car.speed}, Lenkwinkel: {car.steering_angle}")
+    time.sleep(2)
+
+    car.drive(-30, 135)
+    print(f"8 Sekunden rechts rückwärts. Geschwindigkeit: {car.speed}, Lenkwinkel: {car.steering_angle}")
+    time.sleep(8)
+
+    car.drive(-30, 90)
+    print(f"1 Sekunde rückwärts zum Startpunkt. Geschwindigkeit: {car.speed}, Lenkwinkel: {car.steering_angle}")
+    time.sleep(1)
+
+    car.stop()
+    print(f"Fahrmodus 2 rechts beendet.")
+
+def test_fahrmodus_2_links(car):
+    print('Starte Fahrmodus 2 (Links/Gegen Uhrzeigersinn)')
+    car.stop()
+    time.sleep(1)
+
+    car.drive(30)
+    print(f"1 Sekunde vorwärts. Geschwindigkeit: {car.speed}, Lenkwinkel: {car.steering_angle}")
+    time.sleep(1)
+
+    car.drive(30, 45)
+    print(f"8 Sekunden links vorwärts. Geschwindigkeit: {car.speed}, Lenkwinkel: {car.steering_angle}")
+    time.sleep(8)
+
+    car.stop()
+    print(f"Kurzer Zwischenstopp. Geschwindigkeit: {car.speed}, Lenkwinkel: {car.steering_angle}")
+    time.sleep(2)
+
+    car.drive(-30, 45)
+    print(f"8 Sekunden links rückwärts. Geschwindigkeit: {car.speed}, Lenkwinkel: {car.steering_angle}")
+    time.sleep(8)
+
+    car.drive(-30, 90)
+    print(f"1 Sekunde rückwärts zum Startpunkt. Geschwindigkeit: {car.speed}, Lenkwinkel: {car.steering_angle}")
+    time.sleep(1)
+
+    car.stop()
+    print(f"Fahrmodus 2 links beendet.")
+
 if __name__ == '__main__':
-    print('Hier mal die main')
+    print('Testfahrt beginnt')
  
     car1 = BaseCar()
-    car1.drive(50)
-    time.sleep(5)
-    car1.drive(60, 45)
-    time.sleep(1)
-    car1.stop()
-
+    test_fahrmodus_1(car1)
+    #test_fahrmodus_2_rechts(car1)
+    #test_fahrmodus_2_links(car1)
