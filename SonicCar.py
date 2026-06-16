9731a3afbf6a495d70014cb96a0b7dde274b5cb3
jan91vw <jan.krietemeier@volkswagen.de>
2026-06-16 11:23:31 +0200

    Hir SonicCar

---
+++ SonicCar.py
@@ -0,0 +1,28 @@
+from basisklassen import BackWheels, FrontWheels, Ultrasonic
+from BaseCar import *
+import time
+
+class SonicCar(BaseCar):
+
+    def __init__(self):
+        self.us = Ultrasonic()
+
+    def get_distance(self):
+        ''' Error types:
+            -1: Low signal and timeout reached
+            -2: High signal and timeout reached
+            -3: Negative distance
+            -4: Error in time measurement '''
+        return self.us.distance()
+    
+    def stop_car(self):
+        self.get_distance()
+
+
+
+
+if __name__ == '__main__':
+    print('Hier mal die main')
+ 
+    car1 = SonicCar()
+    car1.get_distance()
