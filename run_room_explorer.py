38420648ff633348974ebb110d1a3f4626bf0b3e
dake84 <dake84@gmail.com>
2026-06-26 09:11:59 +0100

    Added run_room_explorer.py

--- run_room_explorer.py
+++ run_room_explorer.py
@@ -3,22 +3,28 @@ import logging
 import threading
 
 import Driving
-from SonicCar import SonicCar
+import InfraredSensor
+from SensorCar import SensorCar
 import UltrasonicSensor
 import logging_setup
 
 if __name__ == '__main__':
     logging_setup.setup_project_logging(logging.INFO)
-    sc = SonicCar()
+    sc = SensorCar()
     sc.stop()
 
-    mode_one = Driving.ModeOne(car=sc)
     us = UltrasonicSensor.UltrasonicSensor(sc)
+    ir = InfraredSensor.InfraredSensor(sc)
+    
+    mode_one = Driving.ModeOne(car=sc)
     dc = Driving.RoomExplorer(car=sc)
     ao = Driving.ApproachObstacle(car=sc)
+    fl = Driving.FollowLine(car=sc)
     
     stop_event = threading.Event()
     us_sensor_thread = threading.Thread(target=us.read_loop, args=[stop_event])
+    ir_sensor_thread = threading.Thread(target=ir.read_loop, args=[stop_event])
+
 
     try:
         input("Mode one... <ENTER>")        
@@ -31,12 +37,21 @@ if __name__ == '__main__':
         input("Approach obstacle... <ENTER>")
         ao.start()
         input("press <Enter> to stop")
-        ao.stop()
+        ao.stop()        
 
         input("Room-Explorer... <ENTER>")
         dc.start()
-        input("<ENTER> zum Beenden")
+        input("press <Enter> to stop")
         dc.stop()
+
+        ir_sensor_thread.start()
+        input("Follow-Line... <ENTER>")
+        fl.start()
+        input("press <Enter> to stop")
+        fl.stop()
+
+
+        input("<ENTER> zum Beenden")
     except KeyboardInterrupt:
         print("Beende Programm")
     finally:
@@ -47,4 +62,5 @@ if __name__ == '__main__':
 
         print("Ending sensor thread...", end="")
         us_sensor_thread.join()
+        ir_sensor_thread.join()
         print("...ended!")
